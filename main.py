71ea3140c64da543e47b59c5d451cbc72347209d
Evelīna Geikina <118215390+evelina1701@users.noreply.github.com>
2023-02-16 14:06:18 +0000

    -

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
+            
             # Process opening bracket, write your code here
             opening_brackets_stack.append(Bracket(next,i))
             #pass
